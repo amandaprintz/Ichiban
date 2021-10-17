6d91807ae92e5195368062556c11c781a9ec4797
amandaprintz <fannyamandakarlsson@gmail.com>
2021-10-17 23:53:20 +0200

    imagesandfonts

---
+++ footer.php
@@ -0,0 +1,1 @@
+<p> <?php echo date("Y"); ?> Mandi :) </p>
--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?= 'The Elephpent in the Room'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="My first project" />
+    <meta property="og:description" content="This is my first HTML class" />
+    <title>First assignment</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <div class="header">
+        <h1>My Website</h1>
+        <p>What am I doing, seriously</p>
+    </div>
+
+
+</body>
+
+</html>
+
+
+<?php require __DIR__ . '/footer.php' ?>
