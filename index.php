2e3055de780954623085230216fb99f386056490
amandaprintz <fannyamandakarlsson@gmail.com>
2021-10-18 09:38:33 +0200

    deletedimages

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:title" content="My first project" />
     <meta property="og:description" content="This is my first HTML class" />
     <title>First assignment</title>
@@ -15,9 +15,35 @@
 
     <div class="header">
         <h1>My Website</h1>
-        <p>What am I doing, seriously</p>
+        <p>What am I doing :(</p>
     </div>
 
+    <main>
+        <article>
+            <img src="images/sashimi.jpeg" alt="Kristen Bell" />
+            <h2>food</h2>
+            <p>Anna</p>
+        </article>
+
+        <article>
+            <img src="images/gyoza.jpeg" alt="Idina Menzel" />
+            <h2>food</h2>
+            <p>Elsa</p>
+        </article>
+
+        <article>
+            <img src="images/chef.jpeg" alt="Jonathan Groff" />
+            <h2>more food</h2>
+            <p>Kristoff</p>
+        </article>
+
+        <article>
+            <img src="images/matcha.jpeg" alt="Josh Gad" />
+            <h2>moooooore food</h2>
+            <p>Olaf</p>
+        </article>
+
+    </main>
 
 </body>
 
